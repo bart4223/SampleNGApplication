25d0abbeddee9f64059de6d31e386979bfecb2b6
Nils <nilsgrimmer@gmx.de>
2013-10-19 17:39:28 +0200

    Graphics.Point2D

---
+++ src/Uniwork/Graphics/Point2D.java
@@ -0,0 +1,24 @@
+package Uniwork.Graphics;
+
+public class Point2D {
+
+    protected Integer FX;
+    protected Integer FY;
+
+    public void setX(Integer aValue) {
+        FX = aValue;
+    }
+
+    public Integer getX() {
+        return FX;
+    }
+
+    public void setY(Integer aValue) {
+        FY = aValue;
+    }
+
+    public Integer getY() {
+        return FY;
+    }
+
+}
