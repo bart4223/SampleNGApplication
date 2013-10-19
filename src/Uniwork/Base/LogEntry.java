db99568ee04e29b221ebfea0434f2516ab267b1e
Nils <nilsgrimmer@gmx.de>
2013-10-19 16:32:46 +0200

    LogEntry

---
+++ src/Uniwork/Base/LogEntry.java
@@ -0,0 +1,32 @@
+package Uniwork.Base;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class LogEntry {
+
+    protected Date FDate;
+    protected String FText;
+
+    public LogEntry(Date aDate, String aText) {
+        FDate = aDate;
+        FText = aText;
+    }
+
+    public Date GetDate() {
+        return FDate;
+    }
+
+    public String GetText() {
+        return FText;
+    }
+
+    public String GetDateAsString() {
+        DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+        formatter.setTimeZone(TimeZone.getTimeZone("GMT+2:00"));
+        return formatter.format(FDate);
+    }
+
+}
