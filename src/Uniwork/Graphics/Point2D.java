6da185779a018da3ee7da236d368d0fab0922492
Nils <nilsgrimmer@gmx.de>
2013-10-19 17:41:22 +0200

    Point2D Constructor

--- src/Uniwork/Graphics/Point2D.java
+++ src/Uniwork/Graphics/Point2D.java
@@ -5,6 +5,16 @@ public class Point2D {
     protected Integer FX;
     protected Integer FY;
 
+    public Point2D() {
+        FX = 0;
+        FY = 0;
+    }
+
+    public Point2D(Integer aX, Integer aY) {
+        FX = aX;
+        FY = aY;
+    }
+
     public void setX(Integer aValue) {
         FX = aValue;
     }
