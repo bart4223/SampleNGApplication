61b28b74cfcb0ed6bd7d92611f5381497b37de7f
Nils <nilsgrimmer@gmx.de>
2013-12-01 17:31:35 +0100

    little Changes

--- src/Uniwork/Visuals/SevenSegmentDisplay.java
+++ src/Uniwork/Visuals/SevenSegmentDisplay.java
@@ -3,17 +3,30 @@ package Uniwork.Visuals;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Control;
-import javafx.scene.layout.Pane;
 import javafx.util.Callback;
 import java.io.IOException;
+import java.net.URL;
 
 public class SevenSegmentDisplay extends Control {
+
+    private final String resourcePath = "%s.fxml";
 
     protected Node FView;
     protected SevenSegmentDisplayController FController;
 
+    private String getViewPath() {
+        return String.format(resourcePath, this.getClass().getSimpleName());
+    }
+
+    private URL getViewURL() {
+        return this.getClass().getResource(this.getViewPath());
+    }
+
     public SevenSegmentDisplay() {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("SevenSegmentDisplay.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader();//getClass().getResource("SevenSegmentDisplay.fxml"));
+        fxmlLoader.setLocation(this.getViewURL());
+        fxmlLoader.setRoot(this);
+        //fxmlLoader.setController(this);
         fxmlLoader.setControllerFactory(new Callback<Class<?>, Object>() {
             @Override
             public Object call(Class<?> param) {
@@ -22,7 +35,6 @@ public class SevenSegmentDisplay extends Control {
         });
         try {
             FView = (Node)fxmlLoader.load();
-
         } catch (IOException ex) {
         }
         getChildren().add(FView);
