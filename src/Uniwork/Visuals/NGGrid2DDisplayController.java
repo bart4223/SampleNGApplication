e8aac5beb5f0e3bbe8d99ae416d58ccfafd006d5
Nils <nilsgrimmer@gmx.de>
2014-06-09 23:02:39 +0200

    Enable scrolling Bugfix layer-combobox with workaround

--- src/Uniwork/Visuals/NGGrid2DDisplayController.java
+++ src/Uniwork/Visuals/NGGrid2DDisplayController.java
@@ -18,31 +18,31 @@ public class NGGrid2DDisplayController extends NGDisplayController {
         double y = 0 - getViewPositionY();
         Integer index = 0;
         Color color;
-        for(double i = x; i <= x + FWidth; i = i + GridDistance) {
+        for(double i = x; i <= x + GridWidth; i = i + GridDistance) {
             FGC.beginPath();
             FGC.moveTo(i, y);
-            FGC.lineTo(i, FWidth);
+            FGC.lineTo(i, GridHeight);
             if (index%2 == 0)
                 color = GridColor.darker();
             else
                 color = GridColor;
             FGC.setStroke(color);
-            FGC.setLineWidth(1);
+            FGC.setLineWidth(GridLineWidth);
             FGC.stroke();
             FGC.closePath();
             index = index + 1;
         }
         index = 0;
-        for(double i = y; i < y + FHeight; i = i + GridDistance) {
+        for(double i = y; i < y + GridHeight; i = i + GridDistance) {
             FGC.beginPath();
             FGC.moveTo(x, i);
-            FGC.lineTo(FWidth, i);
+            FGC.lineTo(GridWidth, i);
             if (index%2 == 0)
                 color = GridColor.darker();
             else
                 color = GridColor;
             FGC.setStroke(color);
-            FGC.setLineWidth(1);
+            FGC.setLineWidth(GridLineWidth);
             FGC.stroke();
             FGC.closePath();
             index = index + 1;
@@ -58,10 +58,16 @@ public class NGGrid2DDisplayController extends NGDisplayController {
         GridDistance = 1;
         GridColor = Color.BLACK;
         DrawGrid = true;
+        GridHeight = FHeight;
+        GridWidth = FWidth;
+        GridLineWidth = 1;
     }
 
     public int GridDistance;
     public Color GridColor;
     public boolean DrawGrid;
+    public double GridWidth;
+    public double GridHeight;
+    public int GridLineWidth;
 
 }
