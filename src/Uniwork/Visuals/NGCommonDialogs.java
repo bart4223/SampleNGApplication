da3bd74b0f540d346c8b33271e368e402c65922e
Nils <nilsgrimmer@gmx.de>
2014-06-15 21:10:35 +0200

    cleanup

--- src/Uniwork/Visuals/NGCommonDialogs.java
+++ src/Uniwork/Visuals/NGCommonDialogs.java
@@ -7,6 +7,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
