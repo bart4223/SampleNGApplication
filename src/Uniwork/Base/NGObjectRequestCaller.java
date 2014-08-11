6fad3a6dce66255d2cc8ef9accff92f77f3a5fc1
Nils <nilsgrimmer@gmx.de>
2014-08-11 21:10:15 +0200

    ObjectRequestCaller MimicORBAction

--- src/Uniwork/Base/NGObjectRequestCaller.java
+++ src/Uniwork/Base/NGObjectRequestCaller.java
@@ -1,10 +1,27 @@
 package Uniwork.Base;
 
+import Uniwork.Misc.NGLogManager;
+
 public class NGObjectRequestCaller extends NGObject {
 
     protected NGObjectRequestInvoker FInvoker;
+    protected NGLogManager FLogManager;
     protected String FObjectName;
     protected String FObjectMethod;
+
+    protected void writeLog(String aText) {
+        writeLog(0, aText);
+    }
+
+    protected void writeLog(int aLogLevel, String aText) {
+        if (FLogManager != null) {
+            FLogManager.writeLog(aLogLevel, aText, getClass().getName());
+        }
+    }
+
+    protected void writeError(String aMethodName, String aErrorText) {
+        writeLog(String.format("<<<ERROR>>> at [%s.%s] with exception [%s]!", getClass().getName(), aMethodName, aErrorText));
+    }
 
     protected void DoInvoke() {
         NGObjectRequestItem item = new NGObjectRequestItem(FObjectName, FObjectMethod);
@@ -14,6 +31,9 @@ public class NGObjectRequestCaller extends NGObject {
     public NGObjectRequestCaller(NGObjectRequestInvoker aInvoker) {
         super();
         FInvoker = aInvoker;
+        FLogManager = null;
+        FObjectName = "";
+        FObjectMethod = "";
     }
 
     public NGObjectRequestInvoker getInvoker() {
@@ -38,6 +58,14 @@ public class NGObjectRequestCaller extends NGObject {
 
     public void Invoke() {
         DoInvoke();
+    }
+
+    public NGLogManager getLogManager() {
+        return FLogManager;
+    }
+
+    public void setLogManager(NGLogManager aLogManager) {
+        FLogManager = aLogManager;
     }
 
 }
