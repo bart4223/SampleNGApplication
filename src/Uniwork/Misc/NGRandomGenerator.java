1b04f9381d4c07ccc6eb56301e98e3eab4a3a24b
Nils <nilsgrimmer@gmx.de>
2014-10-25 14:37:24 +0200

    NGRandomGenerator

---
+++ src/Uniwork/Misc/NGRandomGenerator.java
@@ -0,0 +1,30 @@
+package Uniwork.Misc;
+
+import Uniwork.Base.NGObject;
+
+import java.util.Random;
+
+public class NGRandomGenerator extends NGObject {
+
+    protected Random FRandom;
+
+    public static NGRandomGenerator GlobalRandomGenerator = new NGRandomGenerator();
+
+    public NGRandomGenerator() {
+        super();
+        FRandom = new Random();
+    }
+
+    public Boolean getBoolean() {
+        return FRandom.nextBoolean();
+    }
+
+    public Integer getInteger(Integer aMaxValue) {
+        return FRandom.nextInt(aMaxValue);
+    }
+
+    public Integer getInteger() {
+        return FRandom.nextInt();
+    }
+
+}
