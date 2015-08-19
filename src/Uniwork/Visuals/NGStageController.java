2a36d3eb20dade63f5f50aeb3ff124811d42f7b0
Nils <nilsgrimmer@gmx.de>
2015-08-19 21:44:07 +0200

    Little fix for ORB call

--- src/Uniwork/Visuals/NGStageController.java
+++ src/Uniwork/Visuals/NGStageController.java
@@ -7,17 +7,26 @@ import Uniwork.Misc.NGStrings;
 
 public class NGStageController extends NGCustomStageController {
 
+    protected String getObjectRequestName(String aObject) {
+        String res = aObject;
+        if (FStageItem != null && !aObject.equals("Application")) {
+            String name = NGStrings.getFirstString(FStageItem.getName(), ".");
+            res = NGStrings.addString(name, aObject, ".");
+        }
+        return res;
+    }
+
     protected void Invoke(NGObjectRequestItem aRequest) {
         NGApplication.Application.Invoke(aRequest);
     }
 
     protected void Invoke(String aObject, String aMethod) {
-        String object = aObject;
-        if (FStageItem != null && !aObject.equals("Application")) {
-            String name = NGStrings.getFirstString(FStageItem.getName(), ".");
-            object = NGStrings.addString(name, aObject, ".");
-        }
-        NGApplication.Application.Invoke(object, aMethod);
+        NGApplication.Application.Invoke(getObjectRequestName(aObject), aMethod);
+    }
+
+    protected NGObjectRequestItem newObjectRequest(String aObject, String aMethod) {
+        NGObjectRequestItem res = new NGObjectRequestItem(getObjectRequestName(aObject), aMethod);
+        return res;
     }
 
     public NGStageController() {
