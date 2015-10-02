adddfa1556974044a8b61b3565f88e3e14099bf7
Nils <nilsgrimmer@gmx.de>
2015-10-02 21:02:16 +0200

    Labeled link

---
+++ src/Uniwork/Misc/NGMisc.java
@@ -0,0 +1,19 @@
+package Uniwork.Misc;
+
+public final class NGMisc {
+
+    public static Double Min(Double aA, Double aB) {
+        if (aA <= aB)
+            return aA;
+        else
+            return aB;
+    }
+
+    public static Double Max(Double aA, Double aB) {
+        if (aA >= aB)
+            return aA;
+        else
+            return aB;
+    }
+
+}
