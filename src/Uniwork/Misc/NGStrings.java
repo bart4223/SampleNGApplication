15f7a94eef71683ee1aceac83dd4f178d9481614
Nils <nilsgrimmer@gmx.de>
2017-08-13 17:25:11 +0200

    Comment

--- src/Uniwork/Misc/NGStrings.java
+++ src/Uniwork/Misc/NGStrings.java
@@ -29,7 +29,7 @@ public final class NGStrings {
         // Regex Examples
         // "," use ","
         // "." use "\\."
-        // Position from 1..
+        // Position from 1...
         String[] strs = aValue.split(aRegex);
         if (strs.length > aPosition - 1) {
             return strs[aPosition - 1];
@@ -43,7 +43,7 @@ public final class NGStrings {
         // Regex Examples
         // "," use ","
         // "." use "\\."
-        // Position from 1..
+        // Position from 1...
         String[] strs = aValue.split(aRegex);
         return strs.length;
     }
